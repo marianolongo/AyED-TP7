20d7a0aaa1097601612b46d092dab61ce1311d23
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2018-04-19 18:13:03 -0300

    iso

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,79 +1,86 @@
+import javafx.collections.transformation.SortedList;
+
 public class BinaryTree<T> {
 
-    private class DoubleNode<T>{
+    private class DoubleNode<T> {
         T elem;
-        DoubleNode <T> right, left;
-        public DoubleNode(T o){
+        DoubleNode<T> right, left;
+
+        public DoubleNode(T o) {
             elem = o;
         }
 
-        public DoubleNode(T o, DoubleNode<T> left, DoubleNode<T> right){
+        public DoubleNode(T o, DoubleNode<T> left, DoubleNode<T> right) {
             elem = o;
             this.right = right;
             this.left = left;
         }
     }
+
     private DoubleNode<T> root;
 
-    public BinaryTree(){
+    public BinaryTree() {
         root = null;
     }
-    public BinaryTree(T root){
-        this.root = new DoubleNode <T>(root);
+
+    public BinaryTree(T root) {
+        this.root = new DoubleNode<T>(root);
     }
-    public BinaryTree(T root, BinaryTree<T> tleft, BinaryTree<T> tright){
-        this.root = new DoubleNode<T>(root,tleft.root, tright.root);
+
+    public BinaryTree(T root, BinaryTree<T> tleft, BinaryTree<T> tright) {
+        this.root = new DoubleNode<T>(root, tleft.root, tright.root);
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return root == null;
     }
 
-    public T getRoot(){
+    public T getRoot() {
         return root.elem;
     }
 
-    public BinaryTree<T> getLeft(){
+    public BinaryTree<T> getLeft() {
         BinaryTree<T> t = new BinaryTree<T>();
         t.root = root.left;
         return t;
     }
 
-    public BinaryTree<T> getRight(){
+    public BinaryTree<T> getRight() {
         BinaryTree<T> t = new BinaryTree<T>();
         t.root = root.right;
         return t;
 
     }
 
-    public int weight(){
-        if(isEmpty()){
+    public int weight() {
+        if (isEmpty()) {
             return 0;
         }
         return 1 + getLeft().weight() + getRight().weight();
     }
 
-    public int leaves(){
-        if (getLeft() == null && getRight() == null){
+    public int leaves() {
+        if (getLeft() == null && getRight() == null) {
             return 1;
         }
-        return  getRight().leaves() + getLeft().leaves();
+        return getRight().leaves() + getLeft().leaves();
     }
 
-    public int contains(T elem){
-        if(isEmpty()){
+    public int contains(T elem) {
+        if (isEmpty()) {
             return 0;
         }
-        if(root.elem == elem){
-            return 1 + getLeft().contains(elem) +getRight().contains(elem);
+        if (root.elem == elem) {
+            return 1 + getLeft().contains(elem) + getRight().contains(elem);
         }
         return getLeft().contains(elem) + getRight().contains(elem);
     }
 
     public int elementsInLevel(int level) {
-        if (level == 1){
+        if (level == 1) {
             if (root.elem != null) {
                 return 1;
-            }else
+            } else
                 return 0;
         }
 
@@ -89,24 +96,25 @@ public class BinaryTree<T> {
         return Math.max(hLeftSub, hRightSub) + 1;
     }
 
-    public int sum(){
-        if(root.elem instanceof Integer) throw new RuntimeException("T is not Integer");
-        if(isEmpty())return 0;
+    public int sum() {
+        if (root.elem instanceof Integer) throw new RuntimeException("T is not Integer");
+        if (isEmpty()) return 0;
         return getLeft().sum() + getRight().sum();
     }
 
-    public int sumIfMultipleOf3(){
-        if(root.elem instanceof Integer) throw new RuntimeException("T is not Integer");
-        if(isEmpty() || (Integer)root.elem % 3 != 0)return 0;
+    public int sumIfMultipleOf3() {
+        if (root.elem instanceof Integer) throw new RuntimeException("T is not Integer");
+        if (isEmpty() || (Integer) root.elem % 3 != 0) return 0;
         return getLeft().sum() + getRight().sum();
     }
-    public boolean myEquals(BinaryTree<T> tree){
+
+    public boolean myEquals(BinaryTree<T> tree) {
         return equalsNode(this.root, tree.root);
     }
 
-    private boolean equalsNode(DoubleNode<T> node1, DoubleNode<T> node2){
-        if (node1 == null && node2 == null)return true;
-        if (node1 == null || node2 == null)return false;
+    private boolean equalsNode(DoubleNode<T> node1, DoubleNode<T> node2) {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null) return false;
         if (node1.elem != node2.elem) return false;
 
         return equalsNode(node1.left, node2.left) && equalsNode(node1.right, node2.right);
@@ -114,13 +122,20 @@ public class BinaryTree<T> {
     }
 
     public boolean isomorphic(BinaryTree<T> tree) {
+        if(this.myEquals(tree)){
+            return true;
+        }
         return privateIsomorphic(this.root, tree.root);
     }
 
     private boolean privateIsomorphic(DoubleNode<T> node1, DoubleNode<T> node2) {
-        if(node1 == null && node2 == null)return true;
-        if(node1 == null || node2 == null)return false;
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null) return false;
 
-        return privateIsomorphic(node1.left,node2.left) && privateIsomorphic(node1.right,node2.right);
+        return privateIsomorphic(node1.left, node2.left) && privateIsomorphic(node1.right, node2.right);
+    }
+
+    public boolean resembling(BinaryTree<T> tree){
+
     }
 }
